242b98822f8f86347a6c65681b05e8b0ca71d400
reddavis <reddavis@gmail.com>
2010-10-20 18:55:36 +0100

    commit so I dont delete stuff

---
+++ example/test.rb
@@ -0,0 +1,3 @@
+require "../lib/constructor"
+
+Constructor.construct("tester")
--- lib/constructor.rb
+++ lib/constructor.rb
@@ -1,3 +1,58 @@
+$:.unshift(File.expand_path("../", __FILE__))
+
+require "erubis"
+
 module Constructor
-  # Your code goes here...
+  module_function
+
+  def construct(lib_name)
+    @lib_name = lib_name
+
+    run_bundler
+    construct_rake_file
+    construct_specs
+  end
+
+  public :construct
+
+  def run_bundler
+    system("bundle gem #{@lib_name}")
+  end
+
+  def construct_rake_file
+    File.new("#{@lib_name}/RakeFile") do |file|
+      file.write(template("rake_file"))
+    end
+  end
+
+  def construct_specs
+    # Directories
+    FileUtils.mkdir_p(spec_path + "/#{@lib_name}")
+
+    # Fixture directory
+    FileUtils.mkdir_p(spec_path + "/fixtures")
+
+    # Spec helper
+    File.new("#{space_path}/spec_helper.rb") do |file|
+      file.write(erubis("spec_helper"))
+    end
+
+    # .rspec
+    File.new("#{@lib_name}/.rspec") do |file|
+      file.write("--color")
+      file.write("--profile")
+    end
+  end
+
+  def spec_path
+    File.join(@lib_name, "spec")
+  end
+
+  def erubis(template_name)
+    TinyEruby.new(template(template_name)).result(binding())
+  end
+
+  def template(name)
+    File.read(File.expand_path("../", __FILE__) + "/constructor/templates/#{name}.erb")
+  end
 end
